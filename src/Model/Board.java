48f1db7017d6dd4f187842742f5ca170660a99c1
Ibrahim Said <ibrahimsaid3@cmail.carleton.ca>
2021-10-22 12:21:38 -0400

    small changes to fix bugs

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -4,25 +4,25 @@ import java.util.HashMap;
 
 public class Board {
 
-    private HashMap<Integer, Square> board = new HashMap<>();
+    private HashMap<Integer, Square> board;
 
-    public Board(String description){
-        board = new Hashmap<>();
+    public Board(){
+        board = new HashMap<>();
         buildBoard();
     }
 
     public void buildBoard(){
-        Square GO = new Square;
+        Square GO = new Square("Go!");
         Property property1 = new Property("Carleton University", "brown", 200);
         Property property2 = new Property("123 Street", "brown", 200);
-        Square empty = new Square;
-        Square empty2 = new Square;
+        Square empty = new Square("");
+        Square empty2 = new Square("");
         Property property3 = new Property("321 Street", "blue", 500);
-        Square empty3 = new Square;
-        Square empty4 = new Square;
+        Square empty3 = new Square("");
+        Square empty4 = new Square("");
         Property property4 = new Property("Mansion", "purple", 700);
         Property property5 = new Property("Airport", "purple", 700);
-        Square empty5 = new Square;
+        Square empty5 = new Square("");
 
         board.put(0,GO);
         board.put(1,property1);
@@ -37,5 +37,9 @@ public class Board {
         board.put(9,property5);
         board.put(9,empty5);
 
+    }
+
+    public HashMap<Integer, Square> getBoard() {
+        return board;
     }
 }
