80b477e3fc6e5f1fc47514341e2fab6c45e80058
John <johnafolayan11@yahoo.ca>
2021-10-22 13:50:22 -0400

    incremented hashmap in board class to start from index 1.

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -41,8 +41,8 @@ public class Board {
         board.put(8,empty4);
         board.put(9,property4);
         board.put(10,property5);
-        board.put(10,property5);
-        board.put(10,empty5);
+        board.put(11,property5);
+        board.put(12,empty5);
 
     }
 
