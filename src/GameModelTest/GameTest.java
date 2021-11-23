de7dd1d442b0f451edc750e37ea1653dd252e5c1
John <johnafolayan11@yahoo.ca>
2021-11-22 20:56:42 -0500

    Nearly finished implementation of M3.

--- src/GameModelTest/GameTest.java
+++ src/GameModelTest/GameTest.java
@@ -1,9 +1,10 @@
-/*
 package GameModelTest;
+
 
 import Model.Game;
 import Model.Player;
 
+import Model.Property;
 import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
@@ -22,58 +23,72 @@ public class GameTest {
     @Test
     public void testCreationOfPLayers(){
         game.createPlayers(3);
-        assertEquals(3, game.getHumanPlayers().size());
+        assertEquals(3, game.getPlayers().size());
     }
 
     @Test
-    public void testNumOfInitializedPlayers(){
-        game.initializePlayers(2);
-        assertEquals(2, game.getHumanPlayers().size());
+    public void testNumOfInitializedAIPlayers(){
+        game.initializePlayers(2, 4);
+        assertEquals(6, game.getPlayers().size());
     }
 
     @Test
     public void testThePassingOfATurn(){
-        game.startGame(3);
+        game.initializePlayers(2,0);
         game.passTurn();
         assertEquals(2, game.getCurrentPlayer().getPlayerNumber());
     }
 
     @Test
     public void testSettingAPlayerPosition(){
-        game.startGame(3);
+        game.initializePlayers(3, 4);
         game.setCurrentPlayerPosition(5);
         assertEquals(5, game.getCurrentPlayerPosition());
     }
 
-    */
-/*
     @Test
-    public void testPromptingPlayerPurchase(){
-        game.startGame(3);
-        game.moveToken();
-        assertEquals(1, game.getCurrentPlayer().getOwnedProperties().size());
+    public void testingBuyingOfPlayerProperty(){
+        game.initializePlayers(2,0);
+        game.getCurrentPlayer().setPosition(1);
+        game.getCurrentPlayer().decrementBalance(((Property) game.getBoard().getIndex(game.getCurrentPlayer().getPosition())).getValue());
+        assertEquals(1440, game.getCurrentPlayer().getBalance());
     }
-     *//*
+
+    @Test
+    public void testingTaxingOfPlayer(){
+        game.initializePlayers(2,0);
+        game.checkSquare(4);
+        game.makePurchase();
+        game.getCurrentPlayer().setPosition(4); // set placement of player to income tax placement on board
+
+        assertEquals(1300, game.getCurrentPlayer().getBalance());
+    }
+
+    @Test
+    public void testingOwnershipOfProperty(){
+        Property property = new Property("Property", "gold", 123);
+        game.initializePlayers(2,0);
+        game.getCurrentPlayer().addProperty(property);
+
+        assertTrue(game.propertyOwned(property));
+    }
 
 
     @Test
     public void testRemovalOfBankruptPlayer(){
-        game.startGame(3);
+        game.initializePlayers(2,4);
         game.getCurrentPlayer().setBalance(-100);
         game.removeBankruptPlayer();
-        assertEquals(2, game.getHumanPlayers().size());
+        assertEquals(5, game.getPlayers().size());
     }
-*/
-/*
+
     @Test
     public void testSearchForAWinner(){
-        game.startGame(2);
+        game.initializePlayers(2, 0);
         game.getCurrentPlayer().setBalance(-100);
         game.checkPlayerBalance(game.getCurrentPlayer());
         assertEquals(1, game.getPlayers().size());
-    }*//*
-
+    }
 
 
 }
-*/
