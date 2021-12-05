0334737aef0b9b0c4b51b8c0fec99897457bdcd7
Ibrahim Said <ibrahimsaid3@cmail.carleton.ca>
2021-12-04 20:36:19 -0500

    empty push

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -137,10 +137,8 @@ public class Controller implements ActionListener, Serializable {
                     gameModel.setNumberOfHumanPlayers(numberOfHumanPlayers);
                     gameModel.setNumberOfAIPlayers(numberOfAIPlayers);
                     gameModel.setTotalNumberOfPlayers(totalPlayerAmount);
-                    customBoardChoice= (String) gameStuff.get(7);
-                    gameModel.setCustomBoard(customBoardChoice);
-                    gameView.setBackground();
-
+                    Board board = (Board) gameStuff.get(7);
+                    gameModel.setBoard(board);
 
                     gameView.repaint();
                     gameView.setFeedbackArea("Previous game has been loaded\n" + "\nCurrently turn of: Player " + gameModel.getCurrentPlayer().getPlayerNumber() + "\n");
